a144b4320508a54bbec61451796ac197e1641140
huichuan <lhc14124908@163.com>
2019-03-16 17:34:32 +0100

    add sorttesthelpper

--- ch1/selection sort/selectionSort.java
+++ ch1/selection sort/selectionSort.java
@@ -30,40 +30,10 @@ public class selectionSort {
     public static void main(String[] args) {
 
         // 测试Integer
-        Integer[] a = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-        selectionSort.sort(a);
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i]);
-            System.out.print(' ');
-        }
-        System.out.println();
+        Integer[] arr = SortTestHelper.generateRandomArray(10, 0, 100000);
+        selectionSort.sort(arr);
+        SortTestHelper.printArray(arr);
+        System.out.println(SortTestHelper.isSorted(arr));
 
-        // 测试Double
-        Double[] b = {4.4, 3.3, 2.2, 1.1};
-        selectionSort.sort(b);
-        for (int i = 0; i < b.length; i++) {
-            System.out.print(b[i]);
-            System.out.print(' ');
-        }
-        System.out.println();
-
-        // 测试String
-        String[] c = {"D", "C", "B", "A"};
-        selectionSort.sort(c);
-        for (int i = 0; i < c.length; i++) {
-            System.out.print(c[i]);
-            System.out.print(' ');
-        }
-        System.out.println();
-
-        // 测试自定义的类 Student
-        Student[] d = new Student[4];
-        d[0] = new Student("D", 90);
-        d[1] = new Student("C", 100);
-        d[2] = new Student("B", 95);
-        d[3] = new Student("A", 95);
-        selectionSort.sort(d);
-        for (int i = 0; i < d.length; i++)
-            System.out.println(d[i]);
     }
 }

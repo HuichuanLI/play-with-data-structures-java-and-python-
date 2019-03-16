10cdea16291a44cb570e2abe21bf0ed4dc363620
huichuan <lhc14124908@163.com>
2019-03-16 17:38:26 +0100

    add test function

--- ch1/selection sort/selectionSort.java
+++ ch1/selection sort/selectionSort.java
@@ -35,5 +35,17 @@ public class selectionSort {
         SortTestHelper.printArray(arr);
         System.out.println(SortTestHelper.isSorted(arr));
 
+        // 测试 学生类
+        Student[] d = new Student[4];
+        d[0] = new Student("D",90);
+        d[1] = new Student("C",100);
+        d[2] = new Student("B",95);
+        d[3] = new Student("A",95);
+        selectionSort.sort(d);
+        for( int i = 0 ; i < d.length ; i ++ )
+            System.out.println(d[i]);
+
+
+
     }
 }
